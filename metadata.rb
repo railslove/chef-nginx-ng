ccffaca988fd0e7c4f0711a11dfb81d6ea7fd7f4
Lars Brillert <lars@railslove.com>
2014-10-22 17:46:01 +0200

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "lars@railslove.com"
 license          "Apache 2.0"
 description      "Installs/Configures nging-ng"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.7.0"
+version          "0.8.0"
 name             "nginx_ng"
