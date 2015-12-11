6d575dbec501104931222c3c14d41ab7b3327ede
Jakob Hilden <jakobhilden@gmail.com>
2015-12-11 14:57:31 +0100

    adjusted blacklist

--- attributes/default.rb
+++ attributes/default.rb
@@ -56,7 +56,7 @@ set[:nginx_ng][:ssl_session_timeout] = "5m"
 set[:nginx_ng][:ssl_protocols] = ["TLSv1", "TLSv1.1", "TLSv1.2"]
 set[:nginx_ng][:ssl_ciphers]   = ["ECDHE-RSA-AES128-GCM-SHA256", "ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-RSA-AES256-GCM-SHA384", "ECDHE-ECDSA-AES256-GCM-SHA384", "DHE-RSA-AES128-GCM-SHA256", "DHE-DSS-AES128-GCM-SHA256", "kEDH+AESGCM", "ECDHE-RSA-AES128-SHA256", "ECDHE-ECDSA-AES128-SHA256", "ECDHE-RSA-AES128-SHA", "ECDHE-ECDSA-AES128-SHA", "ECDHE-RSA-AES256-SHA384", "ECDHE-ECDSA-AES256-SHA384", "ECDHE-RSA-AES256-SHA", "ECDHE-ECDSA-AES256-SHA", "DHE-RSA-AES128-SHA256", "DHE-RSA-AES128-SHA", "DHE-DSS-AES128-SHA256", "DHE-RSA-AES256-SHA256", "DHE-DSS-AES256-SHA", "DHE-RSA-AES256-SHA", "AES128-GCM-SHA256", "AES256-GCM-SHA384", "AES128-SHA256", "AES256-SHA256", "AES128-SHA", "AES256-SHA", "AES", "CAMELLIA", "DES-CBC3-SHA", "!aNULL", "!eNULL", "!EXPORT", "!DES", "!RC4", "!MD5", "!PSK", "!aECDH", "!EDH-DSS-DES-CBC3-SHA", "!EDH-RSA-DES-CBC3-SHA", "!KRB5-DES-CBC3-SHA"]
 set[:nginx_ng][:ssl_prefer_server_ciphers] = "on"
-set[:nginx_ng][:format_blacklist] = [".php", ".asp", ".jsp", "cgi", "myadmin", "webdav"]
+set[:nginx_ng][:format_blacklist] = [".php", ".asp", ".jsp", ".cgi", "cgi-bin", "myadmin", "webdav"]
 set[:nginx_ng][:log_formats] = {
   "graylog" => "$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" <msec=$msec|connection=$connection|connection_requests=$connection_requests|millis=$request_time>"
 }
